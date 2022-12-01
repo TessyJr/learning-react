612974e2afcf638bdb20e8a053f66c3f7744f5a2
TessyJr <christopher.nathanael1217@gmail.com>
2022-12-01 13:00:14 +0700

    [add](files): add answer file

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <User name="Tessy" age={19} email="tessy@email.com" />
-      <User name="Mark" age={20} email="mark@email.com" />
-      <User name="Jake" age={21} email="jake@email.com" />
+      <Job salary={90000} position="Senior SDE" company="Amazon" />
+      <Job salary={12000} position="Junior SDE" company="Google" />
+      <Job salary={10000} position="Project Manager" company="Netflix" />
     </div>
   );
 }
 
-const User = (props) => {
+const Job = (props) => {
   return (
     <div>
-      <h1>{props.name}</h1>
-      <h1>{props.age}</h1>
-      <h1>{props.email}</h1>
+      <h1>{props.salary}</h1>
+      <h1>{props.position}</h1>
+      <h1>{props.company}</h1>
     </div>
   );
 };
