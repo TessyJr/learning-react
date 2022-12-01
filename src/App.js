7aefe829e5443ed5f7cf8b8348e19bacdc75731d
TessyJr <christopher.nathanael1217@gmail.com>
2022-12-01 12:56:11 +0700

    [add](files): add files for episode 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <User name="Tessy" age={19} email="tessy@email.com" />
+      <User name="Mark" age={20} email="mark@email.com" />
+      <User name="Jake" age={21} email="jake@email.com" />
+    </div>
+  );
 }
+
+const User = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
+      <h1>{props.email}</h1>
+    </div>
+  );
+};
 
 export default App;
